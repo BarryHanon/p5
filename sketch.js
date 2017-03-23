ade53c69fc5d9f8f5020a6b938f907c81fa45b63
Barry Hanon <barry.hanon@mdycehs.net>
2017-03-23 14:22:56 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+    createCanvas(900, 900);
+}
+
+function draw() {
+    stroke(93, 108, 226);
+    strokeWeight(7);
+    fill(66, 217, 244, .5);
+    ellipse(150, 150, 160, 160);
+    stroke(7, 7, 7);
+    strokeWeight(7);
+    fill(66, 217, 244, .5);
+    ellipse(320, 150, 160, 160);
+    stroke(224, 20, 20);
+    strokeWeight(7);
+    fill(66, 217, 244, .5);
+    ellipse(490, 150, 160, 160);
+    stroke(82, 226, 15);
+    strokeWeight(7);
+    fill(66, 217, 244, .5);
+    ellipse(400, 250, 160, 160);
+    stroke(255, 255, 0);
+    strokeWeight(7);
+    fill(66, 217, 244, .5);
+    ellipse(230, 250, 160, 160);
+}
